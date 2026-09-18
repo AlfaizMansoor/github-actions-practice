0fdea87cefd91bb39a1dc6571eb854d2a16b1c6b
devuqaab <devuqaab@gmail.com>
2026-09-18 17:39:04 +0530

    TASK-5 Run Real Tests in CI error fix v.10.2

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def health_check():
 
 if __name__ == "__main__":
     # Run the application locally in debug mode
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=True)
